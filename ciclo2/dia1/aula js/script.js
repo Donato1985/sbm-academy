6f07ab1c1521edb0c8a6807a1a86f1588d482a58
Donato Luz <donatoluz1985@gmail.com>
2021-08-16 23:50:34 -0300

    pela primeira vez atualizando remotamente de maneira devida

---
+++ ciclo2/dia1/aula js/script.js
@@ -0,0 +1,70 @@
+/*
+è assim que é criado comentários no javascript (lembra o do css)
+1ª Aula de js - 16/08/2021
+
+*/
+/*
+
+var nome = "Donato";//string
+
+console.log(nome);
+
+console.log(typeof(nome));
+
+var nome = 12; //number
+
+console.log(nome);
+
+console.log(typeof(nome));
+
+//concatenar valores/string
+
+document.write("Idade " +nome+"<br>");
+
+*/
+
+//
+
+var fruta = "Laranja";
+
+var oque = "doce";
+
+var tudo = fruta+" "+oque;
+
+//console.log(tudo);
+
+var valor1 = parseInt('1');
+
+var valor1 = '2';
+
+var valor2 = 2;
+
+var total = valor1+valor2;
+
+document.write(valor1+valor2+"\n");
+
+console.log(valor1+valor2);
+
+document.write(total+"\n");
+
+console.log(valor1 - valor2);
+
+//comparadores
+var v = (valor1 == valor2);
+
+console.log(v);
+
+var v = (valor1 === valor2);
+
+var i = 224;
+
+var j = 2;
+
+console.log(i%j);
+
+//multiplicação
+var x = 3;
+
+var y = 2;
+
+console.log(3*2);
---
+++ ciclo2/dia1/desafio/script.js
@@ -0,0 +1,46 @@
+/*
+1. Ao abrir o navegador deverá apresentar uma janela para que digite o nome;
+2. O nome digitado deverá aparecer na segunda janela que irá pedir um número;
+3. Será impresso o nome conforme no exemplo do desafio;
+4. Será impresso o valor digitado entre parênteses;
+5. Irá retornar um valor de comparação resultando em um booleano;
+6. Será efetuado a soma;
+7. Irá retornar o resto da divisão;
+8. Irá retornar o quadrado do valor digitado;
+
+tks
+;)
+
+*/
+var n = 2;
+
+var nome = prompt("Digite o seu nome:");
+
+var num = parseInt(prompt(nome+", digite agora um número para comparar com 'n'"));
+
+var n = num;
+
+document.write("Seja bem vindo(a) "+nome+"<br>");
+
+document.write("Você digitou o número ("+num+")<br>");
+
+var compare = num===n;
+
+document.write("O retorno da comparação booleana é...: "+compare+"<br>");
+
+var soma = n+num;
+document.write("A soma dos valores é...: "+soma+"<br>");
+
+var sub = num-n;
+
+document.write("A subtração dos valores é...: "+sub+"<br>");
+
+var resto = num%n;
+
+document.write("O resto de divisão é de...: "+resto+"<br>");
+
+var quad = num**2;
+
+document.write("O quadrado do número digitado é...: "+quad+"<br>");
+
+document.write(16*16);

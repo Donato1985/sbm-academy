e72da47ec4a619ae827f8caa1fb779c0d24716ab
Donato Luz <donatoluz1985@gmail.com>
2021-08-19 20:29:19 -0300

    Mudanças geradas com sucesso por Donato

---
+++ ciclo2/dia4/aula-extra/script.js
@@ -0,0 +1,25 @@
+
+/*let x=0;
+
+while(x<=3){
+	document.write("<h1>"+x+"</h1><br>");
+	x++;
+}
+*/
+/*
+function cela(){
+	while(true){
+		msg=prompt("Você entrou na cela, digite a senha e poderá sair");
+		if(msg=="a senha"){
+			alert("parabéns, você saiu da cela")
+			break;
+		}else{
+			alert("Você continua aqui, huahuahuahua")
+		}
+	}
+}
+
+cela();
+*/
+
+//document.write("<h1>"+12/0+"</h1><br>");

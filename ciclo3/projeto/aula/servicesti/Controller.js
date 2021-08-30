8c0345fea0bdc1553387f89ec281b1b6f60278a5
Donato Luz <donatoluz1985@gmail.com>
2021-08-30 19:41:28 -0300

    Commit da aula 4 do ciclo 3

--- ciclo3/projeto/aula/servicesti/Controller.js
+++ ciclo3/projeto/aula/servicesti/Controller.js
@@ -50,6 +50,113 @@ app.post('/pedidos', async(req,res)=>{
 });
 
 
+app.get('/listaservicos',async(req,res)=>{
+	await servico.findAll({
+		order: [['nome','DESC']]
+	}).then(function(servicos){
+		res.json({servicos});
+	});
+
+});
+
+app.get('/ofertas',async(req,res)=>{
+	await servico.count('id').then(function(servicos){
+		res.json(servicos);
+	});
+});
+
+
+app.get('/servico/:id',async(req,res)=>{
+	servico.findByPk(req.params.id)
+	.then(servico =>{
+		return res.json({
+			error: false,
+			servico
+		}).catch(function(erro){
+			return res.status(400).json({
+				error:true,
+				message:"Código não está cadastrado!"
+			});
+		});
+	});
+});
+
+
+app.get('/clientes', async(req,res)=>{
+	//exercíco 1 do dia 4
+	await cliente.findAll({
+		order: [['nome','DESC']]
+	}).then(function(clientes){
+		res.json({clientes});
+	});
+});
+app.get('/ordenarclientes',async(req,res)=>{
+	//exercício 2 do dia 4
+	await cliente.findAll({
+		order: [['id','ASC']]
+	}).then(function(clientes){
+		res.json({clientes});
+	});
+});
+
+app.get('/listapedidos',async(req,res)=>{
+	//exercício 3 do dia 4
+	await pedido.findAll({
+		order: [['id','ASC']]
+	}).then(function(pedidos){
+		res.json({pedidos});
+	});
+});
+
+app.get('/pedepormaior',async(req,res)=>{
+	//exercício 4 do dia 4
+	await pedido.findAll({
+		order: [['valor','DESC']]
+	}).then(function(pedidos){
+		res.json(pedidos);
+	});
+});
+
+
+app.get('/countclientes',async(req,res)=>{
+	//exrcício 5 do dia 4
+	await cliente.count('id').then(function(clientes){
+		res.json(clientes);
+	});
+});
+
+app.get('/qtpedidos',async(req,res)=>{
+	//Exercício 6 do dia 4
+	await pedido.count('id').then(function(pedidos){
+		res.json(pedidos);
+	});
+
+});
+
+app.get('/somapornome/:id',async(req,res)=>{
+	//desafio do dia 4 do ciclo 3
+	/*
+	let reqId= await cliente.findAll({
+		attribute: 'id',
+		where: {nome: req.params.nome}
+	});
+	tentei fazer melhor mas ainda preciso saber como fazer isso
+	*/
+
+	pedido.sum('valor',{where: {ClienteId: req.params.id}}).then(soma =>{
+		return res.json({
+			error: false,
+			soma
+		});
+	}).catch(function(erro){
+		return res.status(400).json({
+			error: true,
+			message: "Cliente não encontrado"
+		})
+	})
+
+});
+
 
 let port=process.env.PORT || 3000;
 
